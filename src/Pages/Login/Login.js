0b98e0becfe4b4dc9892c9d2b83786a3a6c9ce36
RobinTheRedLight <robinmitra789@gmail.com>
2023-07-02 11:09:12 +0600

    Admin Route Added

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -4,16 +4,23 @@ import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../contexts/AuthProvider';
 import { GoogleAuthProvider } from 'firebase/auth';
 import { toast } from 'react-hot-toast';
+import useToken from '../../hooks/useToken';
 
 const Login = () => {
   const { register, formState: { errors }, handleSubmit } = useForm();
   const { signIn, providerLogin } = useContext(AuthContext);
   const googleProvider = new GoogleAuthProvider();
   const [loginError, setLoginError] = useState('');
+  const [loginUserEmail, setLoginUserEmail] = useState('');
+  const [token] = useToken(loginUserEmail);
   const location = useLocation();
   const navigate = useNavigate();
 
   const from = location.state?.from?.pathname || '/';
+
+  if (token) {
+    navigate(from, { replace: true });
+  }
 
   const handleGoogleSignIn = () => {
     providerLogin(googleProvider)
@@ -37,7 +44,8 @@ const Login = () => {
       .then(result => {
         const user = result.user;
         console.log(user);
-        navigate(from, { replace: true });
+        setLoginUserEmail(data.email);
+
       })
       .catch(error => {
         console.log(error.message)
